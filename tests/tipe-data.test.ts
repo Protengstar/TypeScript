10007bbeeb780bffb51c0307995e2886bfaea7b2
Protengstar <musthofasifaulq@gmail.com>
2024-03-19 12:16:30 +0700

    Babel typescript

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -4,6 +4,11 @@ describe('Data Type', function() {
         const name: string = "MUSTHOFA SIFA'UL Q";
         const balance: number = 140306;
         const isVip: boolean = true;
+    
+    // GUNAKAN COMMAND : npc tsc --watch
+        // name = 100;
+        // name = "budi";
+        // name = 100;
 
         console.info(name);
         console.info(balance);
