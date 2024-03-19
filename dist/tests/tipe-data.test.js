678c8a7e13e200b9ee2dc3e00860893fbabfcb54
Protengstar <musthofasifaulq@gmail.com>
2024-03-19 11:38:45 +0700

    Type data primitif

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data Type', function () {
+    it('should must declare', function () {
+        const name = "MUSTHOFA SIFA'UL Q";
+        const balance = 140306;
+        const isVip = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,12 @@
+describe('Data Type', function() {
+    it('should must declare', function() {
+        
+        const name: string = "MUSTHOFA SIFA'UL Q";
+        const balance: number = 140306;
+        const isVip: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    })
+})
