8e75688f60e1833f22c3bf4053810a02cdb3d9ae
Protengstar <musthofasifaulq@gmail.com>
2024-03-20 10:22:13 +0700

    Object type update

--- dist/tests/object-type.test.js
+++ dist/tests/object-type.test.js
@@ -1,13 +1,13 @@
 "use strict";
 describe('Object', function () {
-    const person = {
-        id: "1",
-        name: "musthofa",
-        description: "Hello world"
-    };
-    console.info(person);
-    person.id = "2",
-        person.name = "sifaul";
-    person.description = "Say Hello";
-    console.info(person);
+    it('should support in typescript', function () {
+        const person = {
+            id: "1",
+            name: "musthofa",
+        };
+        console.info(person);
+        person.id = "2",
+            person.name = "sifaul";
+        console.info(person);
+    });
 });
--- tests/object-type.test.ts
+++ tests/object-type.test.ts
@@ -1,14 +1,14 @@
 describe('Object', function() {
-    const person: {id: string, name: string, description: string, hobbies?: string} = {
-        id: "1",
-        name: "musthofa",
-        description: "Hello world"
-    };
-    console.info(person);
-
-    person.id = "2",
-    person.name = "sifaul";
-    person.description = "Say Hello"
-
-    console.info(person);
-})
+    it('should support in typescript', function() {
+        const person: {id: string, name: string, hobbies?: string[]} = {
+            id: "1",
+            name: "musthofa",
+        };
+        console.info(person);
+    
+            person.id = "2",
+            person.name = "sifaul";
+    
+        console.info(person);
+    });
+});
