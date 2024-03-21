f94babc482c3a59a5d09f8216f99000c73556d92
Protengstar <musthofasifaulq@gmail.com>
2024-03-21 10:05:03 +0700

    While loop

--- tests/loop.test.ts
+++ tests/loop.test.ts
@@ -1,7 +1,7 @@
 describe('For Loop', function() { 
     it('should support for loop', function() {
         const names : string[] = ['musthofa', 'sifaul', 'qulub'];
-
+        
         for(let i = 0; i < names.length; i++) {
             console.info(names[i]);
         }
@@ -10,6 +10,13 @@ describe('For Loop', function() {
         }
         for(const index in names) {
             console.info(names[index]);
+        }
+    });
+    it('should support while loop', function() {
+        let counter:number = 0;
+        while(counter < 10) {
+            console.info(counter);
+            counter++;
         }
     });
 })
