19318d37aa8432ae965632b352521c58e2bbdd0f
Protengstar <musthofasifaulq@gmail.com>
2024-03-21 09:51:19 +0700

    Switch statement

--- tests/if.test.ts
+++ tests/if.test.ts
@@ -9,4 +9,24 @@ describe('If Statement', function() {
             console.info('Try Again');
         }
     });
+    it('should support ternary operator', function() {
+        const value = 80;
+        const say = value >= 75 ? "Congratulation" : "Try  Again!";
+        console.info(say);
+    });
+    it('should support switch statement', function() {
+        function sayHello(name: string): string {
+            switch(name) {
+                case 'Musthofa':
+                    return 'Saya Musthofa';
+                case 'Sifaul':
+                    return 'Aku Sfaul';
+                default :
+                    return 'Hello';
+            }
+        }
+        console.info(sayHello('Musthofa'));
+        console.info(sayHello('Sifaul'));
+        console.info(sayHello('Qulub'));
+    });
 });
